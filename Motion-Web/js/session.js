cd22cd2d6652415647189d623840a05c6c604f37
Mitchell <dmitchell2@contactusinc.com>
2018-08-28 13:02:36 -0400

    Web end with login page

---
+++ Motion-Web/js/session.js
@@ -0,0 +1,35 @@
+class Session {
+
+	constructor() {
+		this.session = Cookies.get('sessionId');
+	}
+
+	clearSession() {
+	    Cookies.remove('sessionId');
+	    this.session = undefined;
+	}
+
+	createSession(username, password, accountId) {
+		if (this.session != undefined) {
+			alert("Please log out first!");
+			return;
+		}
+
+		$.ajax({
+        url: '/api/auth/createSession',
+        type: 'POST',
+        data: {
+          username: username,
+          password: password,
+          account: accountId
+        },
+        success: function(data) {
+        	Cookies.set('sessionId', data);
+          document.location = '/';
+        },
+        error: function(xhr, status, error) {
+        	alert(error);
+        }
+      });
+	}
+}
